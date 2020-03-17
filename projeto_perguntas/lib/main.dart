25f640a3c05060887a82ab8c9d7efbb4c1fdb49e
Renato Vieira de Souza <renatosouza@clsoftware.com.br>
2020-03-17 16:20:10 -0300

    Adicionado arquivo de anotações e iniciado desenvolvimento app perguntas

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(PerguntaApp());
+main() => runApp(PerguntaApp());
 
-}
-
+//Stateless = sem estado
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {   
+
+    final perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual é o seu animal favorito?"
+    ];
+
+    void responder(){
+      print("Pergunta respondida");
+    }
+
+  
+
     return MaterialApp(
-      home: Text("Olá Flutter")
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text("Resposta 1"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta 2"),
+              onPressed: () {
+                print("Função do tipo Arrow Function");
+              },
+            ),
+            RaisedButton(
+              child: Text("Resposta 3"),
+              onPressed: () => {
+                print("Função do tipo Arrow Function")
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
