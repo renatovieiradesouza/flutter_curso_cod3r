5a5daef0e781c482aed5fa1fa5669af37d39cab8
Renato Souza <renatovieiradesouza1@gmail.com>
2020-03-17 07:04:27 -0300

    Iniciado app, primeiro widget

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,4 +1,16 @@
-main(){
- 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+main(){
+  runApp(PerguntaApp());
+
+}
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {   
+    return MaterialApp(
+      home: Text("Olá Flutter")
+    );
+  }
 }
